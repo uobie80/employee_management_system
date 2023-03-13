c2c770511d6ce511b0b25dd54aa7866e4cb51a33
Uchenna Obicheta <uobicheta@hotmail.com>
2023-03-12 22:08:51 -0400

    Updated README.md file.

---
+++ utils/Employee.js
@@ -0,0 +1,11 @@
+class Employee {
+    constructor(firstname, lastname, ) {
+      this.name = name;
+      this.age = age;
+      this.gender = gender;
+    }
+  
+    greet() {
+      console.log(`Hi, my name is ${this.name}`);
+    }
+  }
